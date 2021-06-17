0e107fa8fe40f646cda3faef7d374c871f019332
alexstrel <astrel@fnal.gov>
2021-06-17 15:40:59 -0500

    minor correction

--- test/build_test.sh
+++ test/build_test.sh
@@ -1,3 +1,3 @@
 nvcc -O0 -g -std=c++17 --expt-relaxed-constexpr -I../build/include -I../build/include/targets/cuda -o transform_reduce_test.o -c transform_reduce_test.cu
-nvcc -o transform_reduce_test.exe transform_reduce_test.o -L/home/astrel/Work/TR/transform_reduce_quda_v7/build/lib -lquda -lmpi
+nvcc -o transform_reduce_test.exe transform_reduce_test.o -L/home/astrel/Work/TR/tmp/parallel_tr_from_quda/build/lib -lquda -lmpi
 
