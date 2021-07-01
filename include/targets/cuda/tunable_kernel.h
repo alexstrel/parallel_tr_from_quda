0603f0a2932987dab54741cdf60818df5ec7de7c
alexstrel <astrel@fnal.gov>
2021-07-01 14:53:59 -0500

    sync with feature/generic_kernel branch from quda repo

--- include/targets/cuda/tunable_kernel.h
+++ include/targets/cuda/tunable_kernel.h
@@ -4,9 +4,17 @@
 #include <target_device.h>
 #include <kernel_helper.h>
 #include <kernel.h>
-#include <quda_cuda_api.h>
 
 namespace quda {
+
+  /**
+     @brief Wrapper around cudaLaunchKernel
+     @param[in] func Device function symbol
+     @param[in] tp TuneParam containing the launch parameters
+     @param[in] arg Host address of argument struct
+     @param[in] stream Stream identifier
+  */
+  qudaError_t qudaLaunchKernel(const void *func, const TuneParam &tp, const qudaStream_t &stream, const void *arg);
 
   class TunableKernel : public Tunable
   {
