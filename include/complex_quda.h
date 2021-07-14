2ee26a3a28874c02ba6999e4053e13691ebeccf7
alexstrel <astrel@fnal.gov>
2021-07-14 01:07:15 -0500

    sync with feature/generic_kernel

--- include/complex_quda.h
+++ include/complex_quda.h
@@ -434,7 +434,7 @@ template<>
 {
 public:
   typedef float value_type;
-  __host__ __device__ inline complex<float>() {};
+  complex<float>() = default;
   constexpr complex<float>(const float & re, const float& im = float()) :
     float2{re, im} { }
 
@@ -506,7 +506,7 @@ template<>
 {
 public:
   typedef double value_type;
-  __host__ __device__ inline complex<double>() {};
+  complex<double>() = default;
   constexpr complex<double>(const double & re, const double& im = double()) :
     double2{re, im} { }
 
@@ -585,7 +585,7 @@ template <> struct complex<int8_t> : public char2 {
 public:
   typedef int8_t value_type;
 
-  __host__ __device__ inline complex<int8_t>() : char2() {};
+  complex<int8_t>() = default;
 
   constexpr complex<int8_t>(const int8_t &re, const int8_t &im = int8_t()) :
     char2{re, im} { }
@@ -622,7 +622,7 @@ struct complex <short> : public short2
 public:
   typedef short value_type;
 
-  __host__ __device__ inline complex<short>() {};
+  complex<short>() = default;
 
   constexpr complex<short>(const short & re, const short& im = short()) :
     short2{re, im} { }
@@ -658,7 +658,7 @@ struct complex <int> : public int2
 public:
   typedef int value_type;
 
-  __host__ __device__ inline complex<int>() {};
+  complex<int>() = default;
 
   constexpr complex<int>(const int& re, const int& im = int()) :
     int2{re, im} { }
@@ -758,7 +758,6 @@ lhs.real()*rhs.imag()+lhs.imag()*rhs.real());
     __host__ __device__
     inline complex<float> operator/(const complex<float>& lhs, const complex<float>& rhs){
 
-    complex<float> quot;
     float s = fabsf(rhs.real()) + fabsf(rhs.imag());
     float oos = 1.0f / s;
     float ars = lhs.real() * oos;
@@ -775,7 +774,6 @@ lhs.real()*rhs.imag()+lhs.imag()*rhs.real());
     __host__ __device__
     inline complex<double> operator/(const complex<double>& lhs, const complex<double>& rhs){
 
-    complex<double> quot;
     double s = fabs(rhs.real()) + fabs(rhs.imag());
     double oos = 1.0 / s;
     double ars = lhs.real() * oos;
